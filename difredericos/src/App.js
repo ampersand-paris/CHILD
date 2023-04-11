0a5da53b103b8a17fed208ce841a716f4ee4cbc7
ampersand-paris <ampersand.s.paris@gmail.com>
2023-04-11 15:33:52 -0400

    router

--- difredericos/src/App.js
+++ difredericos/src/App.js
@@ -19,15 +19,19 @@ function App() {
     <>
       <div className="container">
         <div id="circle"></div>
-        
+        {/* <div className="margin--block--left">
+        </div> */}
         <div className="t--shirt">
           <div className="button">
           <a href="https://docs.google.com/forms/d/e/1FAIpQLSc-tGogYumelRWhEY_4cUTK8j-YRMQBuc3JRK6t6ysvU7fi5g/viewform?usp=sf_link"><h1>ORDER NOW</h1></a>
           </div>
           <img id="img" className="image" src="/T-shirt.png" />
         </div>
-    
+        {/* <div className="margin--block--right">
+          
+        </div> */}
       </div>
+      
     </>
   );
 }
